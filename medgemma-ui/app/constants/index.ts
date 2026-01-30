c65e1de0adffed70d85f2bb0fd07161ced4d8833
maniyakohli <maniyakohli7@gmail.com>
2026-01-30 08:36:48 -0800

    changed env variables

--- medgemma-ui/app/constants/index.ts
+++ medgemma-ui/app/constants/index.ts
@@ -5,6 +5,12 @@
 export const NEXT_PUBLIC_API_URL =
   process.env.NEXT_PUBLIC_API_URL || "http://127.0.0.1:8000";
 
+console.log(
+  "Current API URL:",
+  process.env.NEXT_PUBLIC_API_URL,
+  NEXT_PUBLIC_API_URL,
+);
+
 // constants/index.ts
 export const getSeverityTheme = (
   severity: "low" | "medium" | "high",
